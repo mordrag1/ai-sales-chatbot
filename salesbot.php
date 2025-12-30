00772a8b332b03f8242af1b48cae86631a8b1b8a
unknown <krokface@gmail.com>
2025-12-31 00:24:10 +0400

    chore: update n8n webhook URL

--- salesbot.php
+++ salesbot.php
@@ -5,7 +5,7 @@ header('Content-Type: application/javascript; charset=utf-8');
 
 echo <<<'JS'
 (function () {
-    const DEFAULT_API_URL = 'https://gicujedrotan.beget.app/webhook/0af0b35a-4ea9-4487-a545-8c4608973c5a';
+    const DEFAULT_API_URL = 'https://gicujedrotan.beget.app/webhook/a60472fc-b4e1-4e83-92c4-75c648b9dd80';
     const scriptTag = document.currentScript || (function () {
         const scripts = document.getElementsByTagName('script');
         return scripts[scripts.length - 1];
